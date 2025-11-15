0a82a5b73041781cbbf79a95f1f85276a4f14496
Mayer Unterberg <mayerbarak@gmail.com>
2025-11-15 20:13:23 +0200

    mapped

--- indexBuilder.py
+++ indexBuilder.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+import re
+
 def main():
     #open the file for parsing
+    #loop over the whole 
     with open("AP_Coll_Parsed_9/AP880212", "r") as file:
         content = file.read()
-        print(content)
-
+    
+    # finds every use of <DOCNO> and </DOCNO> and extracts the text in between
+    docnos = re.findall(r"<DOCNO>(.*?)</DOCNO>", content)
+    
+    counter = 1
+    docMap = {}
+    for docno in docnos:
+        docMap[counter] = docno.strip()
+        counter += 1
+    print(docMap)
 
 if __name__ == "__main__":
     main()
